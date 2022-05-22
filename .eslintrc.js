4811e604a5b354b2258d4d26df5e546f46e6385b
jxl <jxljxufe@163.com>
2022-05-22 11:50:15 +0800

    test:格式化test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,17 @@
+const path = require('path');
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true,
-      "node": true
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended' // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parserOptions: {
+    project: path.resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
   },
-  "extends": "eslint:recommended",
-  "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly",
-      "ENV": true
-  },
-  "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-  },
-  "rules": {
-      "linebreak-style": [
-        "error",
-        "unix"
-      ],
-      "quotes": [
-        "error",
-        "single"
-      ]
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import {printWorld} from './modules/testModule'
+import { printWorld } from './modules/testModule';
 // import './test.css';
-export function add(params:any) {
-    const text = 'text show';
-    console.log(text);
-    return params + 100;
+export function add(params: any) {
+  const text  = 'text show';
+  console.log(text);
+  return params + 100;
 }
-
 console.log('start work...');
-console.log(add(1)); 
-printWorld('jxl')
+console.log(add(1));
+printWorld('jxl');
